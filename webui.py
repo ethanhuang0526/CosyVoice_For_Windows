f64a19b064799bcff33189b99b60d4dfecd54e23
ethanhuang0526 <ethan-huang@hiskio.com>
2024-10-16 12:13:26 +0800

    Update webui.py
    add gradio share=True config

--- webui.py
+++ webui.py
@@ -295,7 +295,7 @@ def main():
                               
         mode_checkbox_group.change(fn=change_instruction, inputs=[mode_checkbox_group], outputs=[instruction_text])
     demo.queue(max_size=4, default_concurrency_limit=2)
-    demo.launch(server_name='0.0.0.0', server_port=args.port,inbrowser=True)
+    demo.launch(server_name='0.0.0.0', server_port=args.port,inbrowser=True,share=True)
 
 
 if __name__ == '__main__':
